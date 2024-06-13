10ee6f661f7cd02fa7023b0435d1e6a43b04d8dc
kimsh1691 <kimsh1691@gmail.com>
2024-06-13 16:53:29 +0900

    Article class lombok 적용

--- src/main/java/me/kimsoohyeon/springbootdeveloper/domain/Article.java
+++ src/main/java/me/kimsoohyeon/springbootdeveloper/domain/Article.java
@@ -1,9 +1,14 @@
 package me.kimsoohyeon.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
     @Id
@@ -21,21 +26,5 @@ public class Article {
     public Article(String title, String content){
         this.title = title;
         this.content = content;
-    }
-
-    protected Article(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
