faa508045f03ea2d8aa24149bd24e1cf1ddfc91b
kimsh1691 <kimsh1691@gmail.com>
2024-06-20 14:55:33 +0900

    findAllArticles controller 생성

--- src/main/java/me/kimsoohyeon/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/kimsoohyeon/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,13 @@ package me.kimsoohyeon.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.kimsoohyeon.springbootdeveloper.domain.Article;
 import me.kimsoohyeon.springbootdeveloper.dto.AddArticleRequest;
+import me.kimsoohyeon.springbootdeveloper.dto.ArticleResponse;
 import me.kimsoohyeon.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +22,16 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
