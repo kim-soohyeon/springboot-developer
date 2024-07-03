ccb65d2d046391d511926de34a85fecc00418bde
kimsh1691 <kimsh1691@gmail.com>
2024-07-03 16:26:35 +0900

    조회 컨트롤러 메서드 생성

--- src/main/java/me/kimsoohyeon/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/kimsoohyeon/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.kimsoohyeon.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.kimsoohyeon.springbootdeveloper.domain.Article;
 import me.kimsoohyeon.springbootdeveloper.dto.ArticleListViewResponse;
+import me.kimsoohyeon.springbootdeveloper.dto.ArticleResponse;
 import me.kimsoohyeon.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleResponse(article));
+
+        return "article";
     }
 }
