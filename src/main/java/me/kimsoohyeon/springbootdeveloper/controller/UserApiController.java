0a9818cce3d7118c40b4083bd89adae63939d924
kim-soohyeon <kimsh1691@gmail.com>
2024-07-18 23:50:23 +0900

    로그아웃 메서드 생성

--- src/main/java/me/kimsoohyeon/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/kimsoohyeon/springbootdeveloper/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.kimsoohyeon.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.kimsoohyeon.springbootdeveloper.dto.AddUserRequest;
 import me.kimsoohyeon.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +20,11 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
