54f0e078871183ed1d3a40a1b36abf416fa4c590
kimsh1691 <kimsh1691@gmail.com>
2024-07-31 17:12:37 +0900

    토큰 API 구현 - TokenService> createNewAccessToken 메서드 추가

---
+++ src/main/java/me/kimsoohyeon/springbootdeveloper/service/TokenService.java
@@ -0,0 +1,29 @@
+package me.kimsoohyeon.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.kimsoohyeon.springbootdeveloper.config.jwt.TokenProvider;
+import me.kimsoohyeon.springbootdeveloper.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken){
+        //토큰 유효성 검사 실패시 예외처리
+        if(!tokenProvider.validToken(refreshToken)){
+            throw new IllegalArgumentException("Unexpected token");
+        }
+
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findById(userId);
+
+        return tokenProvider.generateToken(user, Duration.ofHours(2));
+    }
+}
